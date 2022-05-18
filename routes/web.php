33aa0669756a76ae7e30906513b8cdf3a0bf03f4
nillkizz <alx.n.smith@gmail.com>
2022-05-18 21:05:21 +0300

    Add Admin dashboard layout html

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Example Routes
+Route::view('/', 'landing');
+Route::match(['get', 'post'], '/dashboard', function () {
+  return view('admin.dashboard');
 });
+Route::view('/pages/slick', 'admin.pages.slick');
+Route::view('/pages/datatables', 'admin.pages.datatables');
+Route::view('/pages/blank', 'admin.pages.blank');

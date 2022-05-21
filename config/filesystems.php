2c994fc823e95f03818c3613f30828563c669431
nillkizz <alx.n.smith@gmail.com>
2022-05-21 16:57:45 +0300

    Filesystems media diskname fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,7 +36,7 @@ return [
       'throw' => false,
     ],
 
-    'meida' => [
+    'media' => [
       'driver' => 'local',
       'root' => storage_path('app/public/media'),
       'url' => env('APP_URL') . '/storage/media',

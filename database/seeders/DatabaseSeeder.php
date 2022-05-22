e797bfae9bdb456b1ed2ad39f3e7d869dc5b483a
nillkizz <alx.n.smith@gmail.com>
2022-05-22 13:45:39 +0300

    Add admin user to DatabaseSeeder as fixture.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    User::factory()
+      ->state([
+        'name' => 'Administrator',
+        'email' => 'admin@example.com',
+        'password' => Hash::make('password')
+      ])
+      ->create();
+  }
 }

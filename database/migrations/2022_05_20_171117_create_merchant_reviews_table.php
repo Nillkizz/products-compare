1959ad28f428bcaaac0637db2278589c98f70f36
nillkizz <alx.n.smith@gmail.com>
2022-06-03 12:23:59 +0000

    Add merchant reviews table

---
+++ database/migrations/2022_05_20_171117_create_merchant_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\ContactType;
+use App\Models\Merchant;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('merchant_reviews', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+      $table->foreignIdFor(Merchant::class);
+      $table->number('stars');
+      $table->boolean('is_good_service');
+      $table->boolean('is_good_delivery');
+      $table->boolean('is_correspond_description');
+      $table->string('text');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('merchant_contacts');
+  }
+};

ece6c0e2a0c62c9d216c0c64aaffa517d07bee64
nillkizz <alx.n.smith@gmail.com>
2022-06-03 13:14:31 +0000

    Fix fallback preview placeholder

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,10 +21,9 @@ class Product extends Model implements HasMedia
   public function previewUrl($conversion = null)
   {
     $media = $this->getFirstMedia('preview');
-    if ($media == null) return;
+    if ($media == null) return env('FALLBACK_IMAGE_URL');
     if ($conversion == null) return $media->getUrl();
     if ($media->hasGeneratedConversion($conversion)) return $media->getUrl($conversion);
-    return env('FALLBACK_IMAGE_URL');
   }
 
   public function registerMediaCollections(): void

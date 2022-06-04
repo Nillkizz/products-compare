038d8b91b7f5eb062d4486c2fcacc08b33e7e33b
nillkizz <alx.n.smith@gmail.com>
2022-06-04 18:12:24 +0000

    Fix notification text

--- resources/static/admin/js/app.js
+++ resources/static/admin/js/app.js
@@ -1,3 +1,3 @@
 import './bootstrap';
 
-new ClipboardJS('[data-clipboard-text]').on('success', (e) => { Dashmix.helpers('jq-notify', { type: 'success', icon: 'fa fa-check me-1', message: 'Link copied!' }); });
+new ClipboardJS('[data-clipboard-text]').on('success', (e) => { Dashmix.helpers('jq-notify', { type: 'success', icon: 'fa fa-check me-1', message: 'Copied!' }); });
